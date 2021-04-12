64f5ae58371fc2528268a1223ce9c5f75bb49803
Oleg <oleg.ptro737@gmail.com>
2021-04-12 09:16:42 +0300

    Create models

---
+++ src/main/java/com/example/demo/models/Product.java
@@ -0,0 +1,23 @@
+package com.example.demo.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Product { //extends Model
+    @Id
+    private Integer product_id;
+
+    private String color;
+    private String size;
+    private String description;
+    private Integer price;
+    private Integer pieces_in_stock; // количество в наличии
+
+    @ManyToOne
+    private Model model;
+
+}
---
+++ src/main/java/com/example/demo/models/User.java
@@ -0,0 +1,25 @@
+package com.example.demo.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer user_id;
+
+    private String email;
+    private String password;
+    private Boolean is_active; // подтвердил ли почту
+
+    @OneToOne(targetEntity = UserProfile.class)
+    private UserProfile user_profile;
+    @ManyToOne
+    private Role role_id;
+
+
+}
